3632a988d3b10e6f3cd9660b21a491ae269227c8
manikanta8712 <manikanta@igreensystems.com>
2024-11-28 21:48:26 +0530

    small change

--- campuswave/Modules/Student/Controllers/HomeController.php
+++ campuswave/Modules/Student/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends BaseController{
     }
 
     public function index(){
-        echo "Hello, Student dashboard here";
+        echo "Hello, students dashboard here";
         exit;
     }
 
